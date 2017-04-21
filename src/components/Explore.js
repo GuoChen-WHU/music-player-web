9f7af3f3f350b14de24b96a301e342fcb27e6ef1
GuoChen-WHU <496474070@qq.com>
2017-04-21 14:08:03 +0800

    Updates

--- src/components/Explore.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Explore = () => (
-  <div className="container-fluid">
-    <p>猜你喜欢</p>
-  </div>
-);
-
-export default Explore;
--- src/services/config.js
+++
@@ -1,3 +0,0 @@
-const serverUrl = 'http://localhost:3000/';
-
-export const searchUrl = serverUrl + 'api/search/';
